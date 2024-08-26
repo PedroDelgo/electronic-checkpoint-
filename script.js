7787ac8d6c685a444d8c12c31b74f9704b278789
PedroDelgo <pedrodelg.go@gmail.com>
2024-08-26 08:48:58 -0300

    feat: basic structure of html and css

---
+++ script.js
@@ -0,0 +1,9 @@
+const date = new Date();
+
+console.log(date.getDate());
+console.log(date.getMonth() + 1);
+console.log(date.getFullYear());
+
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
